406a1f367243a53a001323483fd45c13572ab868
farasource <github@farasource.com>
2021-07-28 17:21:49 +0430

    nothing

--- ABook/src/main/java/ghasemi/abbas/book/BaseActivity.java
+++ ABook/src/main/java/ghasemi/abbas/book/BaseActivity.java
@@ -1,17 +1,6 @@
 /*
- * Copyright (C) 2019 All rights reserved for FaraSource (ABBAS GHASEMI)
- *
- * All rights This application For FaraSource Reserved.
- * using the license of this source is permitted only for customers.
- * customers are not permitted to share,sell and exchange this source.
- *
- * This source is sold only at the following site:
- *
+ * Copyright (C) 2019  All rights reserved for FaraSource (ABBAS GHASEMI)
  * https://farasource.com
- *
- * Copyright ABBAS GHASEMI, 2019.
- * Developer => ABBAS GHASEMI
- *
  */
 package ghasemi.abbas.book;
 
--- ABook/src/main/java/ghasemi/abbas/book/components/ViewPager.java
+++ ABook/src/main/java/ghasemi/abbas/book/components/ViewPager.java
@@ -1,15 +1,6 @@
 /*
  * Copyright (C) 2019  All rights reserved for FaraSource (ABBAS GHASEMI)
- *
- * All rights This application For FaraSource Reserved.
- * using the license of this source is permitted only for customers.
- * customers are not permitted to share,sell and exchange this source.
- *
- * This source is sold only at the following site:
- *
  * https://farasource.com
- *
- * Copyright ABBAS GHASEMI, 2019. Developer => ABBAS GHASEMI
  * Reference = http://stackoverflow.com/
  */
 package ghasemi.abbas.book.components;
